63c3e1a1e1135e66660ac3fdcd699bcebddf6d8a
Floper <43756755+Flop3r@users.noreply.github.com>
2023-11-25 11:46:25 +0100

    Added is_riot_id_valid

--- packages/discord_bot/commands.py
+++ packages/discord_bot/commands.py
@@ -1,8 +1,7 @@
-import json
 import re, requests
 
 from .responses import *
-from ..game_handler.game import add_player
+from ..game_handler.game import *
 from .global_vars import RIOT_API
 
 async def handle_command(message, message_content):
@@ -19,10 +18,12 @@ async def handle_command(message, message_content):
             response = handle_add_command(args, message.guild)
             await message.reply(response, mention_author=False)
 
-        elif command == "json":
-            with open('Data/Players.json', 'r') as file:
-                data = json.load(file)
-            await message.reply(data, mention_author=False)
+        elif command == "get_players_data":
+            await message.reply(get_players_data(), mention_author=False)
+
+        elif command == "reset":
+            reset_players_data()
+            await message.reply(RESETED_RESPONSE, mention_author=False)
 
         else:
             await message.reply(INVALID_COMMAND_RESPONSE, mention_author=False)
@@ -31,44 +32,39 @@ async def handle_command(message, message_content):
         print(e)
 
 def handle_add_command(args, guild) -> str:
-    print(f"Debug: args = {args}")
-    print(f"Debug: args[0] = {args[0]}")
-    print(f"Debug: args[1] = {args[1]}")
-    # Helper function to check if the argument has a valid format
-    def is_valid_arg(arg) -> bool:
+    def is_valid_arg_format(arg) -> bool:
         return bool(re.match(r'<@(\d{18})>', arg))
 
-    # Helper function to check if the argument has a valid format
-    def is_valid_arg2(arg) -> bool:
-        return bool(re.match(r'^.+#\w{4}$', arg))
+    def is_valid_arg2_format(arg) -> bool:
+        return bool(re.match(r'^.+#.+$', arg))
 
-    # Helper function to extract player ID from a valid argument
     def extract_user_id(arg) -> str:
         return re.match(r'<@(\d{18})>', arg).group(1)
 
-
-    # Check if the number of arguments is correct and if the argument is valid
     if len(args) != 2:
         return INVALID_ARGUMENT_RESPONSE
-    if not is_valid_arg(args[0]):
+    if not is_valid_arg_format(args[0]):
         return f"{INVALID_ARGUMENT_RESPONSE}: {args[0]}"
-    if not is_valid_arg2(args[1]):
+    if not is_valid_arg2_format(args[1]):
         return f"{INVALID_ARGUMENT_RESPONSE}: {args[1]}"
 
     user_id = extract_user_id(args[0])
     member = guild.get_member(int(user_id))
 
-    if member and is_valid_riot_id(args[1]):
-        add_player(user_id, args[1])
-        return USER_ADDED_RESPONSE
-    else:
+    game_name, tag_line = args[1].split('#')
+
+    if not member:
         return USER_NOT_FOUND_RESPONSE
+    elif not is_valid_riot_id(game_name, tag_line):
+        return RIOT_USER_NOT_FOUND_RESPONSE
+    else:
+        puuid = get_puuid(game_name, tag_line)
+        add_player(user_id, puuid, game_name, tag_line)
+        return USER_ADDED_RESPONSE
 
 
 
-def is_valid_riot_id(riot_id):
-    game_name, tag_line = riot_id.split('#')
-
+def is_valid_riot_id(game_name, tag_line):
     base_url = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id"
     endpoint = f"{game_name}/{tag_line}"
     url = f"{base_url}/{endpoint}?api_key={RIOT_API}"
@@ -84,3 +80,12 @@ def is_valid_riot_id(riot_id):
     else:
         print(f"Błąd podczas sprawdzania gracza. Kod odpowiedzi: {response.status_code}")
         return False
+
+def get_puuid(game_name, tag_line):
+    base_url = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id"
+    endpoint = f"{game_name}/{tag_line}"
+    url = f"{base_url}/{endpoint}?api_key={RIOT_API}"
+
+    response = requests.get(url)
+    data = json.loads(response.content)
+    return data['puuid']
--- packages/game_handler/game.py
+++ packages/game_handler/game.py
@@ -1,5 +1,5 @@
 import json
-def add_player(discord_id, riot_id= "nickname"):
+def add_player(discord_id, puuid, game_name, tag_line):
     try:
         with open(f'Data/Players.json', 'r') as json_file:
             players_data = json.load(json_file)
@@ -7,10 +7,20 @@ def add_player(discord_id, riot_id= "nickname"):
         players_data = {}
 
     players_data[discord_id] = {
-        'riot_id' : riot_id,
+        'puuid' : puuid,
+        'game_name' : game_name,
+        'tag_line' : tag_line,
         'wins' : 0,
         'loses': 0
     }
 
     with open(f'Data/Players.json', 'w') as json_file:
         json.dump(players_data, json_file, indent=2)
+
+def get_players_data():
+    with open('Data/Players.json', 'r') as file:
+        return json.load(file)
+
+def reset_players_data():
+    with open(f'Data/Players.json', 'w') as json_file:
+        json.dump({}, json_file, indent=2)
